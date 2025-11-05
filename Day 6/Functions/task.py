f307c51df8d0308b5d75720f1e34c71137e85c34
Abrahim Mejía Valdez <abrmeval@outlook.com>
2025-11-04 21:03:34 -0600

    While loops and functions p1

---
+++ Day 6/Functions/task.py
@@ -0,0 +1,16 @@
+#Functions
+
+#Built in functions
+print("Hello")
+num_char = len("Hello")
+print(num_char)
+
+# Custom functions
+
+def my_function():
+ print("Hello")
+print("Bye")
+
+# Calling the custom function
+my_function()
+
