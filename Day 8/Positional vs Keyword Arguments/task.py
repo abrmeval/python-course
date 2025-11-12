8da60e1b53c078d2b1368772392856ab3dffd382
Abrahim Mejía Valdez <abrmeval@outlook.com>
2025-11-11 22:34:53 -0600

    Functions with inpust

--- Day 8/Positional vs Keyword Arguments/task.py
+++ Day 8/Positional vs Keyword Arguments/task.py
@@ -4,5 +4,14 @@ def greet_with_name(name):
     print(f"Hello {name}")
     print(f"How do you do {name}?")
 
-
 greet_with_name("Jack Bauer")
+
+# Functions with more than 1 input
+def greet_with(name, location):
+    print(f"Hello {name}")
+    print(f"What is it like in {location}")
+
+greet_with("Jose", "Somewhere")
+
+#Kekword arguments
+greet_with(location="Somewhere", name="Jose" )
