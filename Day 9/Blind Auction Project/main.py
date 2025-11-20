0c32186c6e2fcd05d6f2d8aad89ef9b52c8e5235
Abrahim Mejía Valdez <abrmeval@outlook.com>
2025-11-19 21:19:47 -0600

    Auction program

--- Day 9/Blind Auction Project/main.py
+++ Day 9/Blind Auction Project/main.py
@@ -3,4 +3,34 @@
 # TODO-3: Whether if new bids need to be added
 # TODO-4: Compare bids in dictionary
 
+from art import logo
+
+print(logo)
+
+bids = {}
+more_bidders = True
+
+while more_bidders:
+    name = input("What is youe name?: ")
+    bid = float(input("What is your bid? $ "))
+    add_more = input("Are there any other bidders? Type 'yes' or  'no'.\n").lower()
+    bids[name] = bid
+
+    #Built in function to get the max value of a dictionary
+    # max(bids, bids.get)
+
+    if add_more == "no":
+        more_bidders = False
+        temp_key = ""
+
+        for key in bids:
+            if temp_key == "":
+                temp_key = key
+            elif bids[key] > bids[temp_key]:
+                temp_key = key
+
+        print(f"The winner is {temp_key} with a bid of {round(bids[temp_key])}")
+    else:
+        print("\n" * 20)
+
 
